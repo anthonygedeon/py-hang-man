0e315f68dee201c88d7f8dd234bb41f943b1b5ef
Anthony Gedeon <anthony.gedeonn@gmail.com>
2019-11-12 16:31:39 -0600

    now prints the winner

--- hang-man.py
+++ hang-man.py
@@ -72,8 +72,8 @@ wrong_letters = []
 loop = True
 points = 0
 
-def hide_word(): # replace each letter with a *
-
+def hide_word():
+    """Loops through the character list and replaces each letter with an * """
     copied_word = copy.copy(characters)
 
     for index, letter in enumerate(copied_word):
@@ -85,7 +85,10 @@ hidden_characters = hide_word()
 guess_word = [characters, hidden_characters] # [ ['h', 'a', 'n', 'g', 'm', 'a', 'n'], ['*', '*', '*', '*', '*', '*', '*'] ]
 
 def get_letter_matches(player_answer):
-    # Player answer will be a single alphabet letter
+    """
+        Check if player input matches any of letters in the given word
+        and returns the position and duplicates of that letter
+    """
     word = guess_word[0] # ['h', 'a', 'n', 'g', 'm', 'a', 'n']
 
     indexes = [index for index in range(len(word)) if word[index] == player_answer] # Don't know exactly what this code does
@@ -94,6 +97,10 @@ def get_letter_matches(player_answer):
     return letter_indexes # {'a': [1, 5]}
 
 def reveal_letters(dict, player_answer):
+    """
+        Takes the player input and if the letter matches the word then it will
+        unhide the letter in the guess_word list
+    """
     hidden_word = guess_word[1]
 
     for index in dict[player_answer]:
@@ -102,10 +109,13 @@ def reveal_letters(dict, player_answer):
     return ''.join(hidden_word) # dict[player_answer] -> [1, 5]
 
 def print_stick_board(number):
+    """Print the right stick man board"""
+
     length = len(number)
     print(stick_man[length])
 
 def get_incorrect_letters(player_answer):
+    """Tally up all the incorrect words"""
     word = guess_word[0]
 
     if player_answer not in word:
@@ -114,6 +124,7 @@ def get_incorrect_letters(player_answer):
     return wrong_letters
 
 def get_point(letter):
+    """Give player 100 points for getting the correct letter"""
     global points
     if letter in characters:
         points += 100
@@ -121,13 +132,20 @@ def get_point(letter):
     else:
         print('Points earned: {}'.format(points))
 
-def is_winner(asterisk):
-    pass
+def is_winner(complete_word, secret_word):
+    """Determine the winner and loser"""
+    global loop
+
+    if secret_word == complete_word:
+        print('You Won!')
+        loop = False
+    #else:
+        # after the board list is exhausted check if the the word matches, if it doesn't then you lost and exist out the loop
 
 def game_loop():
 
     while loop:
-        print('__________________________________________________________________')
+        print('__________________________________________________')
         player_letter = input('Choose a letter: ')
 
         hidden = reveal_letters( get_letter_matches( player_letter ), player_letter )
@@ -138,8 +156,7 @@ def game_loop():
 
         get_point(player_letter)
         print('You\'ve entered (wrong): {}'.format( incorrect_letters ))
-
-    is_winner(hidden)
+        is_winner(''.join(guess_word[0]), hidden)
 
 
 game_loop()
